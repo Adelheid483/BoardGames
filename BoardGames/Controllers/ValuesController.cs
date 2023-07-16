7aaf58915eb8a3d2797fcfcf89d30c79a52e9dee
Alina <adelheid0106@gmail.com>
2023-07-16 19:59:53 +0300

    Initial commit

---
+++ BoardGames/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGames.Controllers;
+
+[Route("api/games")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetGames() {
+        var games = new[]
+        {
+            new {Name = "1"},
+            new {Name = "2"},
+            new {Name = "3"},
+        };
+
+        return Ok(games);
+    }
+}
