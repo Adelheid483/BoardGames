ffc958f024ca7fe704cfb8bf8dced617fc0ec56e
Alina <adelheid0106@gmail.com>
2023-08-01 11:24:50 +0300

    add react-hook-form

--- src/app/api/gamesApi.ts
+++ src/app/api/gamesApi.ts
@@ -1,17 +1,10 @@
 import { GameModel } from "../dataModels/gameModel";
 import { httpGet } from "./requestApi";
-import { TyrantsOfTheUnderdarkModel } from "../dataModels/Games/tyrantsOfTheUnderdarkModel";
 
 const resource = "games";
 
 export async function getGames(): Promise<Array<GameModel>> {
     return httpGet({
         url: `${resource}/list`,
-    });
-}
-
-export async function getTyrantsOfTheUnderdark(): Promise<TyrantsOfTheUnderdarkModel> {
-    return httpGet({
-        url: `${resource}/tyrants-of-the-underdark`,
     });
 }
