0046b74ec8e8f42a092c7cee9675a2e6326e8ac7
Alina <adelheid0106@gmail.com>
2023-08-11 00:53:24 +0300

    remove counter

--- src/app/api/tyrantsOfTheUnderdarkApi.ts
+++ src/app/api/tyrantsOfTheUnderdarkApi.ts
@@ -1,16 +1,8 @@
 import { httpPost } from "./requestApi";
-import { TotalCountModel } from "../dataModels/totalCountModel";
 import { serialize } from "object-to-formdata";
 import { TyrantsOfTheUnderdarkSaveModel } from "../dataModels/tyrantsOfTheUnderdarkSaveModel";
 
 const resource = "tyrants-of-the-underdark";
-
-export async function countTyrantsOfTheUnderdark(model: TotalCountModel): Promise<number> {
-    return httpPost({
-        url: `${resource}/count`,
-        body: serialize(model),
-    });
-}
 
 export async function saveTyrantsOfTheUnderdark(model: TyrantsOfTheUnderdarkSaveModel): Promise<number> {
     return httpPost({
