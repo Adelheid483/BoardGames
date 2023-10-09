f91bdce1404af10028e9bb41226747e4b551b15d
Alina Trushkevich <58606840+Adelheid483@users.noreply.github.com>
2023-10-09 15:30:25 +0300

    Add Users Entity

---
+++ BoardGames.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using BoardGames.Domain.Enums;
+
+namespace BoardGames.Domain.Entities;
+
+public class User : Entity
+{
+    public string Name { get; set; }
+    
+    public string Password { get; set; }
+    
+    public Role Role { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime? UpdatedAt { get; set; }
+
+    public DateTime? PasswordChangedAt { get; set; }
+}
---
+++ BoardGames.Domain/Enums/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardGames.Domain.Enums;
+
+public enum Role
+{
+    [Display(Name = "Admin")]
+    Admin = 0,
+    
+    [Display(Name = "User")]
+    User = 1,
+}
